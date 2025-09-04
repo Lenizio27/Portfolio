b7f37ff40cb112b26613666d8088a2c7326ff4ed
Lenizio27 <lenizio27@gmail.com>
2025-09-04 12:42:29 -0300

    Resumo minha jornada

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Portfolio
+Olá! 👋 Seja bem-vindo ao meu portfólio profissional.
+Este projeto foi desenvolvido com criatividade, dedicação e atenção aos detalhes, com o objetivo de proporcionar uma ótima experiência de navegação e demonstrar, na prática, minhas habilidades técnicas e minha organização.
+
+Espero que este espaço possa servir de inspiração e, caso tenha interesse, será um prazer conversarmos sobre possíveis colaborações ou trabalharmos juntos em novos desafios.
+
 Portfólio pessoal de Lenizio Nascimento
+👉Acesse **[aqui](https://portfolio-lenizionascimento.vercel.app/)**
+
+## Tecnologias
+
+-HTML5
+-CSS3
+-REACT
+-TAILWINDCSS
+-JAVASCRIP
--- portfolio_react/src/Components/Sobre.jsx
+++ portfolio_react/src/Components/Sobre.jsx
@@ -12,13 +12,13 @@ const Sobre = () => {
                         <div className="StyleGrow p-3 md-flex">
                             <h3 className="text-[20px] text-p5">Minha Jornada</h3>
                             <p>
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum doloribus vel facere, et non minus inventore nostrum! Ullam, ad ab repellendus animi repellat illum! Voluptatum possimus laudantium labore debitis harum.
+                                Olá! Sou Lenizio Nascimento (LNZ), um desenvolvedor frontend dedicado e curioso por tecnologia. Iniciei minha trajetória na programação há alguns mêses, venho me aprimorando continuamente, sempre em busca de evoluir minhas habilidades e entregar soluções de qualidade.
                                 <br />
                                 <br />
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit ratione, quia ad illum eaque provident consequuntur dolorem deserunt ab velit error dignissimos dicta! Quod nam nesciunt qui neque tempora fugiat.
+                                Tenho foco em React e desenvolvimento web moderno, prezando pela performance, acessibilidade e boas práticas de código. Gosto de criar interfaces que unam design e funcionalidade, proporcionando experiências que realmente façam diferença para os usuários.
                                 <br />
                                 <br />
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum soluta explicabo, mollitia quae libero fugit tempore odio perspiciatis aperiam! Eum blanditiis minus earum ea non molestiae a tenetur corporis tempore.
+                                tenho interesse por aprender constantemente novas tecnologias, explorar áreas como back-end e mobile, além de dedicar tempo a projetos pessoais que me desafiam e ampliam minha visão como desenvolvedor. Também valorizo a troca de conhecimento e acredito que a colaboração é um dos maiores motores da evolução na área de tecnologia.
                             </p>
                         </div>
                         {/* Experiências */}
@@ -26,7 +26,7 @@ const Sobre = () => {
                             <h3 className="text-[20px] text-p5">Experiência</h3>
                             <div className="flex flex-column justify-evenly items-center w-full h-full">
                                 <div className="xpBox bg-p1 w-full p-2 rounded-md border border-b4">
-                                    <h4>Projeto Drip-Store</h4>
+                                    <h4>Desafio Drip-Store</h4>
                                     <h5>Geração Tech</h5>
                                     <p>Projeto Front-End</p>
                                 </div>
