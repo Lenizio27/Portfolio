3ca84cbe4f801e5a773bc25f47bfaa91d3988cc9
lenizio <lenizio27@gmail.com>
2026-01-24 09:10:54 -0300

    feat: funcionalidade para envios de email criada com sucesso

--- portfolio_react/src/Components/Contatos.jsx
+++ portfolio_react/src/Components/Contatos.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { useRef } from "react";
 import { CardContato } from "./Cards";
+import emailjs from '@emailjs/browser'
 
 const Contatos = () => {
     const cardContatos = [
@@ -20,6 +22,21 @@ const Contatos = () => {
         },
     ]
 
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Substitua pelos seus IDs reais do painel EmailJS
+    emailjs.sendForm('service_gmail_lenizio', 'template_tv2dx27', form.current, 'cLlmCw4rO4sKpJLY_')
+      .then((result) => {
+          alert("Sua mensagem foi enviada com sucesso! 🚀");
+          form.current.reset(); // Limpa o formulário após enviar
+      }, (error) => {
+          alert("Erro ao enviar: " + error.text);
+      });
+  };
     
     return ( 
         <>
@@ -35,37 +52,57 @@ const Contatos = () => {
                         <section className="flex flex-col p-3 bg-b1 rounded-sm flex-1">
                             <h2 className="text-[20px] text-p5 pb-2">Envie uma Mensagem</h2>
                             <form 
-                            className="flex gap-2 w-full flex-wrap" 
+                            className="flex flex-col gap-2 w-full flex-wrap" 
+                            ref={form} onSubmit={sendEmail}
                             >
-                                <label className="flex flex-col flex-1">Nome
-                                    <input
-                                    type="text" 
-                                    className="bg-b3 rounded-sm px-2" 
-                                    placeholder="Seu nome completo"
-                                    />
-                                </label>
-                                <label className="flex flex-col flex-1">Email
-                                    <input 
-                                    type="text" 
-                                    className="bg-b3 rounded-sm px-2" placeholder="seuemail@gmail.com"
-                                    />
-                                </label>
+                                <div className="flex gap-2">
+                                    <label className="flex flex-col flex-1">Nome
+                                        <input
+                                        type="text"
+                                        className="bg-b3 rounded-sm px-2"
+                                        placeholder="Seu nome completo"
+
+                                        name="user_name"
+                                        required
+                                        />
+                                    </label>
+                                    <label className="flex flex-col flex-1">Email
+                                        <input
+                                        type="text"
+                                        className="bg-b3 rounded-sm px-2" placeholder="seuemail@gmail.com"
+
+                                        name="user_email"
+                                        required
+                                        />
+                                    </label>
+                                </div>
+                                <div>
+                                    <label className="flex flex-col flex-1">Assunto
+                                        <input 
+                                        type="text" 
+                                        className="bg-b3 rounded-sm px-2" 
+                                        placeholder="Assunto do Email"
+
+                                        name="subject"
+                                        required
+                                        />
+                                    </label>
+                                    <label className="flex flex-col flex-1">Mensagem
+                                        <textarea 
+                                        type="text" 
+                                        className="bg-b3 rounded-sm px-2" 
+                                        placeholder="Conte sobre suas ideias de projetos.."
+                                        
+                                        name="message"
+                                        rows={4}
+                                        required
+                                        />
+                                    </label>
+                                </div>
+                             <button 
+                             type="submit"
+                             className="w-full h-10 bg-b4 rounded-md hover:bg-b2 transition active:border-1 my-2">Verificar</button>
                             </form>
-                            <form>
-                                 <label className="flex flex-col flex-1">Assunto
-                                    <input 
-                                    type="text" 
-                                    className="bg-b3 rounded-sm px-2" 
-                                    placeholder="Assunto do Email"/>
-                                </label>
-                                <label className="flex flex-col flex-1">Mensagem
-                                    <textarea 
-                                    type="text" 
-                                    className="bg-b3 rounded-sm px-2" 
-                                    placeholder="Conte sobre suas ideias de projetos.."/>
-                                </label>
-                            </form>
-                             <button className="w-full h-10 bg-b4 rounded-md hover:bg-b2 transition active:border-1 my-2">Verificar</button>
                         </section>
                         <section className="flex flex-col p-3 rounded-sm gap-2 flex-1">
                             {
